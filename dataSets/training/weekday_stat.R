b5c88c77d609a332e8dbc6ca72b8cdfd132b576d
Sean1127 <wannaplay1304@gmail.com>
2017-04-01 12:33:32 +0800

    Init commit

---
+++ dataSets/training/weekday_stat.R
@@ -0,0 +1,20 @@
+fullChart = chart
+weekday = //
+for(j in 1:7){}
+for(i in seq(j,91,7)){
+  weekday[j] <- chart[i,];
+}
+}
+
+for(i in 1:91) {
+  for(j){
+    if( == NA){
+      weekday=date(row(chart)[j])
+      var = var(weekday)
+      avg 
+        if(var <= err) {
+          fullChart[j,i] = avg
+        }
+    }
+  }
+}
